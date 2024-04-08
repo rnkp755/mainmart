b66cd7d0c38422f4551de0130f84b1e4a248d9bc
hackerraushan <rnkp755@gmail.com>
2024-04-08 22:32:03 +0530

    Bug Fixes

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -322,7 +322,54 @@ function NavBar({ children }) {
                               >
                                 <span>{category.name}</span>
                               </Popover.Button>
-
+                              <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-200"
+                                enterFrom="opacity-0 translate-y-1"
+                                enterTo="opacity-100 translate-y-0"
+                                leave="transition ease-in duration-150"
+                                leaveFrom="opacity-100 translate-y-0"
+                                leaveTo="opacity-0 translate-y-1"
+                              >
+                                <Popover.Panel className="absolute z-10 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                    <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                                      {category.featured.map((item) => (
+                                        <a
+                                          key={item.name}
+                                          href={item.href}
+                                          className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                                        >
+                                          <img
+                                            src={item.imageSrc}
+                                            alt={item.imageAlt}
+                                            className="flex-shrink-0 h-6 w-6"
+                                          />
+                                          <div className="ml-4">
+                                            <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                          </div>
+                                        </a>
+                                      ))}
+                                    </div>
+                                    <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8 overflow-scroll">
+                                      {category.sections.map((section) => (
+                                        <div key={section.id}>
+                                          <p className="text-base font-medium text-gray-900">{section.name}</p>
+                                          <ul role="list" className="mt-4 space-y-4">
+                                            {section.items.map((item) => (
+                                              <li key={item.name} className="text-sm truncate">
+                                                <a href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
+                                                  {item.name}
+                                                </a>
+                                              </li>
+                                            ))}
+                                          </ul>
+                                        </div>
+                                      ))}
+                                    </div>
+                                  </div>
+                                </Popover.Panel>
+                              </Transition>
 
                             </>
                           )}
